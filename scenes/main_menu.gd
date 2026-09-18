5b5f5c4119bfcb1cd91573f444481d13621623ad
1zz222 <xeriapsila.shaggy@gmail.com>
2026-09-19 01:48:07 +0300

    finalpush

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,12 +1,20 @@
 extends Control
 
-# Αλλάξε τη διαδρομή αν η σκηνή του παιχνιδιού σου έχει διαφορετικό όνομα/φάκελο
-@export var game_scene_path: String = "res://scenes/game.tscn"
+@onready var main_buttons: VBoxContainer = $VBoxContainer
+@onready var score_panel: Control = $Score
+
+func _ready() -> void:
+	main_buttons.show()
+	score_panel.hide()
 
 func _on_start_button_pressed() -> void:
-	# Μετάβαση στη σκηνή του παιχνιδιού
-	get_tree().change_scene_to_file(game_scene_path)
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-func _on_quit_button_pressed() -> void:
-	# Έξοδος από το παιχνίδι
-	get_tree().quit()
+func _on_score_button_pressed() -> void:
+	main_buttons.hide()
+	score_panel.show()
+
+func _on_close_score_button_pressed() -> void:
+	print("Πατήθηκε το Back!")
+	score_panel.hide()
+	main_buttons.show()
